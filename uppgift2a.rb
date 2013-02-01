70f5b292eef2e7cc46e82b5ba82454d5da7945ae
kgm <kgm@kgm.cc>
2013-02-01 18:13:20 +0100

    gives feedback on optimal # of guesses

--- uppgift2a.rb
+++ uppgift2a.rb
@@ -13,6 +13,9 @@ limit = gets.chomp.to_i
 # svaret är ett slumptal mellan 1 och limit, inklusive
 answer = Random.rand(1..limit)
 
+# räkna antal gissningar
+guesses = 1;
+
 # läs in gissning, gå ur loopen omm gissning == svaret
 while( true )
 	print ("Skriv din gissning: ")
@@ -23,7 +26,14 @@ while( true )
 		break
 	end
 
-	puts "Du gissade #{guess}, som är för #{(guess<answer)?"lågt":"högt"}!"
+	puts "Du gissade #{guess}, som är för #{ (guess<answer)? 'lågt': 'högt' }!"
+	guesses +=1;
+
 end
 
-puts answer #debug
+# skriv ut antal gissningar
+puts "Du använde totalt #{guesses} gissningar för att hitta svaret."
+
+# optimalt antal gissningar, från uppgiften log(limit)/log(2)+1, rundat nedåt
+optimal = (Math.log(limit)/Math.log(2)+1).floor
+puts "(Du kunde ha klarat det på #{optimal} gissningar.)" if guesses > optimal
