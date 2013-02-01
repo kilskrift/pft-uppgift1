10628fa81f567c8fba1977f25fd7e1f60a675828
kgm <kgm@kgm.cc>
2013-02-01 15:18:42 +0100

    converts YY-MM-DD => YYYY-MM-DD

--- uppgift1c.rb
+++ uppgift1c.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 #
-# Inlämningsuppgift 1b i Programmering för Testare vt2013
+# Inlämningsuppgift 1c i Programmering för Testare vt2013
 #
 
 # skriv ut "tabellhuvudet", 20 tecken+1+15 tecken
@@ -14,6 +14,20 @@ puts divider
 names = File.open("namn.txt").readlines
 dates = File.open("datum.txt").readlines
 
+# datumen är på formatet YY-MM-DD, men vi vill har fyra siffror för årtal
+# datumen är strängar, och vi kan indexera strängar precis som en array
+# vi vet att om de två första tecknen YY är mellan 0 & 13 är det 2000-talet
+# vi går igenom dates-arrayen & jämför om strängens två första tecken är <13
+# beroende på lägger vi sen till "20" eller "19" längst fram i strängen 
+fulldates = dates.map do |date|
+	if( date[0..1].to_i ) < 13 then
+		"20" + date 
+	else
+		"19" + date
+	end
+end
+
+
 # rad 1 i namn.txt motsvarar rad 1 i datum.txt
 # så plats 1 i names motsvarar plats 1 dates
 # vi går igenom names "rad för rad" med en while-loop
@@ -23,7 +37,7 @@ while( row < names.length )
 	#print nice looking name & date information
 	print names[row].chomp.ljust(20)
 	print "|"
-	puts dates[row].chomp.rjust(15) # puts to get a new line
+	puts fulldates[row].chomp.rjust(15) # puts to get a new line
 
 	#advance to next "row" in arrays
 	row += 1;
