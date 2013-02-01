2b95a5635bb99ba12eba02215f53a8cfbdad002c
kgm <kgm@kgm.cc>
2013-02-01 16:47:04 +0100

    sorted names, fulldates via a hash

--- uppgift1c.rb
+++ uppgift1c.rb
@@ -19,6 +19,7 @@ dates = File.open("datum.txt").readlines
 # vi vet att om de två första tecknen YY är mellan 0 & 13 är det 2000-talet
 # vi går igenom dates-arrayen & jämför om strängens två första tecken är <13
 # beroende på lägger vi sen till "20" eller "19" längst fram i strängen 
+# .map skapar en ny array av returvärdet från det block den kör på arrayen
 fulldates = dates.map do |date|
 	if( date[0..1].to_i ) < 13 then
 		"20" + date 
@@ -27,20 +28,21 @@ fulldates = dates.map do |date|
 	end
 end
 
+# vi vill sortera resultaten efter datum, men eftersom vi har våra värden
+# spridda över två arrayer kan vi förenkla för oss genom att lägga ihop dem
+# i en hash namn => datum
+name_and_date = {} #empty hash
+for i in (0...names.length) # 0 to lenght-1
+	name_and_date[ names[i] ] = fulldates[i] #add to hash w/name as key to date
+end
 
-# rad 1 i namn.txt motsvarar rad 1 i datum.txt
-# så plats 1 i names motsvarar plats 1 dates
-# vi går igenom names "rad för rad" med en while-loop
-# för varje rad skriver vi ut namn & motsvarade datum
-row = 0;
-while( row < names.length )
-	#print nice looking name & date information
-	print names[row].chomp.ljust(20)
+# en hash är till sin natur osorterad, men vi kan returnera en array med sort_by
+# sort_by tar ett block som argument, där kan vi ange att vi vill sortera på datum
+sorted_by_date = name_and_date.sort_by { |name, date| date }
+sorted_by_date.each do |nd|
+	print nd[0].chomp.ljust(20)
 	print "|"
-	puts fulldates[row].chomp.rjust(15) # puts to get a new line
-
-	#advance to next "row" in arrays
-	row += 1;
+	puts nd[1].chomp.rjust(15) # puts to get a new line
 end
 
 
