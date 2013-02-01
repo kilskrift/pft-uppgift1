49f1638749b65e42ec632ca5f23217527b2fd7a0
kgm <kgm@kgm.cc>
2013-02-01 18:29:04 +0100

    bear rush game finished

--- uppgift2b.rb
+++ uppgift2b.rb
@@ -1,39 +1,35 @@
 # encoding: utf-8
 
 #
-# Inlämningsuppgift 2a i Programmering för Testare vt2013
+# Inlämningsuppgift 2b i Programmering för Testare vt2013
 #
 
-puts "Välkommen till gissningsspelet. Kan du gissa talet?"
+puts "Välkommen till Björnjakten!"
 
-# läs in en övre gräns för talet att gissa
-print ("Skriv in övre gräns för talet: ")
-limit = gets.chomp.to_i
+# björnen är 50 till 100 meter bort
+answer = Random.rand(50..100)
 
-# svaret är ett slumptal mellan 1 och limit, inklusive
-answer = Random.rand(1..limit)
-
-# räkna antal gissningar
-guesses = 1;
+puts "Något rusar fram mellan träden ca 50 till 100 meter bort."
 
 # läs in gissning, gå ur loopen omm gissning == svaret
 while( true )
-	print ("Skriv din gissning: ")
+	print ("Du ser en arg björn! Hur långt bort siktar du?: ")
 	guess = gets.chomp.to_i
 
 	if( guess == answer )
-		puts "Rätt gissning, bra!"
+		puts "Du träffar björnen, puh!"
 		break
 	end
 
-	puts "Du gissade #{guess}, som är för #{ (guess<answer)? 'lågt': 'högt' }!"
-	guesses +=1;
+	puts "Du sköt för #{ (guess<answer)? 'kort': 'långt' }!"
 
+	# räkna ut hur nära björnen är
+	rush = Random.rand(10..20)
+	answer = answer - rush
+	unless answer > 0 
+		puts "Björnen kommer ifatt dig. Åh nej..."
+		exit
+	else
+		puts "Björnen rusar #{rush} meter närmare dig!"
+	end
 end
-
-# skriv ut antal gissningar
-puts "Du använde totalt #{guesses} gissningar för att hitta svaret."
-
-# optimalt antal gissningar, från uppgiften log(limit)/log(2)+1, rundat nedåt
-optimal = (Math.log(limit)/Math.log(2)+1).floor
-puts "(Du kunde ha klarat det på #{optimal} gissningar.)" if guesses > optimal
