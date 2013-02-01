ab531433c614e910059de51b31c7fcad9e5a3bfb
kgm <kgm@kgm.cc>
2013-02-01 17:45:23 +0100

    gets guess from player

--- uppgift2a.rb
+++ uppgift2a.rb
@@ -13,5 +13,16 @@ limit = gets.chomp.to_i
 # svaret är ett slumptal mellan 1 och limit, inklusive
 answer = Random.rand(1..limit)
 
+# läs in gissning, gå ur loopen omm gissning == svaret
+while( true )
+	print ("Skriv din gissning: ")
+	guess = gets.chomp.to_i
+
+	puts "Du gissade " + guess.to_s
+
+	if( guess == answer )
+		break
+	end
+end
 
 puts answer #debug
