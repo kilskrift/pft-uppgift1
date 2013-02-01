fa0b9e7da0cdb51c0ec0398c28dba19ee79d0cd1
kgm <kgm@kgm.cc>
2013-02-01 14:37:34 +0100

    solution to exercise 1a

--- uppgift1a.rb
+++ uppgift1a.rb
@@ -3,3 +3,17 @@
 #
 # Inlämningsuppgift 1a i Programmering för Testare vt2013
 #
+
+# läs in namn & datum till två arrayer
+names = File.open("namn.txt").readlines
+dates = File.open("datum.txt").readlines
+
+# rad 1 i namn.txt motsvarar rad 1 i datum.txt
+# så plats 1 i names motsvarar plats 1 dates
+# vi går igenom names "rad för rad" med en while-loop
+# för varje rad skriver vi ut namn & motsvarade datum
+row = 0;
+while( row < names.length )
+	puts names[row].chomp + " " + dates[row].chomp
+	row += 1;
+end
