0b0dc20e5ed0a8883e176988ddfe5b39a2142f67
kgm <kgm@kgm.cc>
2013-02-01 17:41:37 +0100

    gets limit from player

--- uppgift2a.rb
+++ uppgift2a.rb
@@ -4,3 +4,10 @@
 # Inlämningsuppgift 2a i Programmering för Testare vt2013
 #
 
+puts "Välkommen till gissningsspelet. Kan du gissa talet?"
+
+# läs in en övre gräns för talet att gissa
+print ("Skriv in övre gräns för talet: ")
+limit = gets.chomp.to_i
+
+puts limit #debug
